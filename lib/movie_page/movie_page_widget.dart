04eea1729332799d10d85f2e7a6ca1f4a94f1f70
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-05 16:31:35 +0000

    Updating to latest FlutterFlow output.

--- lib/movie_page/movie_page_widget.dart
+++ lib/movie_page/movie_page_widget.dart
@@ -301,6 +301,21 @@ class _MoviePageWidgetState extends State<MoviePageWidget> {
                               highlightColor: Colors.transparent,
                               onTap: () async {
                                 if (widget.movieDoc?.driveType == 'gdrive') {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        'https://www.googleapis.com/drive/v3/files/${widget.movieDoc?.videoUrl}?alt=media&access_token=${FFAppState().googleAccessToken}',
+                                        style: TextStyle(
+                                          color: FlutterFlowTheme.of(context)
+                                              .primaryText,
+                                        ),
+                                      ),
+                                      duration: Duration(milliseconds: 4000),
+                                      backgroundColor:
+                                          FlutterFlowTheme.of(context)
+                                              .secondary,
+                                    ),
+                                  );
                                   await actions.playVideoInExternalPlayer(
                                     'https://www.googleapis.com/drive/v3/files/${widget.movieDoc?.videoUrl}?alt=media&access_token=${FFAppState().googleAccessToken}',
                                   );
