c688e81621a9f00aafe0a7e5286bd82a32107395
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-22 12:11:18 +0000

    Continue Watching card update

--- lib/components/continue_watching_card_widget.dart
+++ lib/components/continue_watching_card_widget.dart
@@ -5,6 +5,7 @@ import '/custom_code/actions/index.dart' as actions;
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
 import 'continue_watching_card_model.dart';
@@ -50,72 +51,99 @@ class _ContinueWatchingCardWidgetState
   Widget build(BuildContext context) {
     context.watch<FFAppState>();
 
-    return InkWell(
-      splashColor: Colors.transparent,
-      focusColor: Colors.transparent,
-      hoverColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-      onTap: () async {
-        await actions.launchExternalPlayer(
-          functions.emptyMovieDoc(),
-          functions.emptyEpisodeDoc(),
-          widget.continueDoc,
-          FFAppState().activeProfileRef!.id,
-          '',
-        );
-      },
-      child: Container(
-        width: 200.0,
-        height: 120.0,
-        decoration: BoxDecoration(),
-        child: Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: CachedNetworkImage(
-                fadeInDuration: Duration(milliseconds: 200),
-                fadeOutDuration: Duration(milliseconds: 200),
-                imageUrl: widget.continueDoc!.imageUrl,
-                width: 200.0,
-                height: 200.0,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.transparent,
-                    FlutterFlowTheme.of(context).primaryBackground
-                  ],
-                  stops: [0.2, 1.0],
-                  begin: AlignmentDirectional(0.0, -1.0),
-                  end: AlignmentDirectional(0, 1.0),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        InkWell(
+          splashColor: Colors.transparent,
+          focusColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: () async {
+            await actions.launchExternalPlayer(
+              functions.emptyMovieDoc(),
+              functions.emptyEpisodeDoc(),
+              widget.continueDoc,
+              FFAppState().activeProfileRef!.id,
+              '',
+            );
+          },
+          child: Container(
+            width: 200.0,
+            height: 120.0,
+            decoration: BoxDecoration(),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: CachedNetworkImage(
+                    fadeInDuration: Duration(milliseconds: 200),
+                    fadeOutDuration: Duration(milliseconds: 200),
+                    imageUrl: widget.continueDoc!.imageUrl,
+                    width: 200.0,
+                    height: 200.0,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-            ),
-            Align(
-              alignment: AlignmentDirectional(0.0, 1.0),
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 10.0),
-                child: LinearPercentIndicator(
-                  percent: functions.calculateProgressRatio(
-                      widget.continueDoc?.watchedSeconds,
-                      widget.continueDoc?.totalSeconds),
-                  lineHeight: 3.0,
-                  animation: true,
-                  animateFromLastPercent: true,
-                  progressColor: FlutterFlowTheme.of(context).primary,
-                  backgroundColor: FlutterFlowTheme.of(context).accent4,
-                  padding: EdgeInsets.zero,
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        FlutterFlowTheme.of(context).primaryBackground
+                      ],
+                      stops: [0.2, 1.0],
+                      begin: AlignmentDirectional(0.0, -1.0),
+                      end: AlignmentDirectional(0, 1.0),
+                    ),
+                  ),
                 ),
-              ),
+                Align(
+                  alignment: AlignmentDirectional(0.0, 1.0),
+                  child: Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 10.0),
+                    child: LinearPercentIndicator(
+                      percent: functions.calculateProgressRatio(
+                          widget.continueDoc?.watchedSeconds,
+                          widget.continueDoc?.totalSeconds),
+                      lineHeight: 3.0,
+                      animation: true,
+                      animateFromLastPercent: true,
+                      progressColor: FlutterFlowTheme.of(context).primary,
+                      backgroundColor: FlutterFlowTheme.of(context).accent4,
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 0.0, 0.0),
+          child: Text(
+            valueOrDefault<String>(
+              widget.continueDoc?.contentType == 'movie'
+                  ? widget.continueDoc?.title
+                  : 'Season ${widget.continueDoc?.season.toString()}, Episode ${widget.continueDoc?.episode.toString()}',
+              'Title',
+            ),
+            style: FlutterFlowTheme.of(context).bodyMedium.override(
+                  font: GoogleFonts.inter(
+                    fontWeight: FontWeight.w600,
+                    fontStyle:
+                        FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                  ),
+                  letterSpacing: 0.0,
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                ),
+          ),
+        ),
+      ],
     );
   }
 }
