d57807c1a53440159fc961566185b0dc5af07083
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-24 10:21:02 +0000

    Updating to latest FlutterFlow output.

--- lib/my_profile/my_profile_widget.dart
+++ lib/my_profile/my_profile_widget.dart
@@ -707,40 +707,6 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
                                         },
                                       ),
                                     ),
-                                  if ((myProfileProfilesRecord != null) ==
-                                      false)
-                                    Align(
-                                      alignment: AlignmentDirectional(0.0, 0.0),
-                                      child: Text(
-                                        'Your List is Empty !',
-                                        style: FlutterFlowTheme.of(context)
-                                            .headlineMedium
-                                            .override(
-                                              font: GoogleFonts.inter(
-                                                fontWeight:
-                                                    FlutterFlowTheme.of(context)
-                                                        .headlineMedium
-                                                        .fontWeight,
-                                                fontStyle:
-                                                    FlutterFlowTheme.of(context)
-                                                        .headlineMedium
-                                                        .fontStyle,
-                                              ),
-                                              color:
-                                                  FlutterFlowTheme.of(context)
-                                                      .accent3,
-                                              letterSpacing: 0.0,
-                                              fontWeight:
-                                                  FlutterFlowTheme.of(context)
-                                                      .headlineMedium
-                                                      .fontWeight,
-                                              fontStyle:
-                                                  FlutterFlowTheme.of(context)
-                                                      .headlineMedium
-                                                      .fontStyle,
-                                            ),
-                                      ),
-                                    ),
                                 ],
                               ),
                             ),
