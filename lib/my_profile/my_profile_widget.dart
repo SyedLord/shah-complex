4920096ec0d82c3864d9b9aebbbb59a086b58ca5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-24 10:24:58 +0000

    Update

--- lib/my_profile/my_profile_widget.dart
+++ lib/my_profile/my_profile_widget.dart
@@ -700,6 +700,51 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
                                                         );
                                                       },
                                                     ),
+                                                  if ((listViewMyListRecord
+                                                              .movieRef ==
+                                                          null) &&
+                                                      (listViewMyListRecord
+                                                              .seasonRef ==
+                                                          null))
+                                                    Align(
+                                                      alignment:
+                                                          AlignmentDirectional(
+                                                              0.0, 0.0),
+                                                      child: Text(
+                                                        'Your List is Empty !',
+                                                        style:
+                                                            FlutterFlowTheme.of(
+                                                                    context)
+                                                                .headlineMedium
+                                                                .override(
+                                                                  font:
+                                                                      GoogleFonts
+                                                                          .inter(
+                                                                    fontWeight: FlutterFlowTheme.of(
+                                                                            context)
+                                                                        .headlineMedium
+                                                                        .fontWeight,
+                                                                    fontStyle: FlutterFlowTheme.of(
+                                                                            context)
+                                                                        .headlineMedium
+                                                                        .fontStyle,
+                                                                  ),
+                                                                  color: FlutterFlowTheme.of(
+                                                                          context)
+                                                                      .accent3,
+                                                                  letterSpacing:
+                                                                      0.0,
+                                                                  fontWeight: FlutterFlowTheme.of(
+                                                                          context)
+                                                                      .headlineMedium
+                                                                      .fontWeight,
+                                                                  fontStyle: FlutterFlowTheme.of(
+                                                                          context)
+                                                                      .headlineMedium
+                                                                      .fontStyle,
+                                                                ),
+                                                      ),
+                                                    ),
                                                 ],
                                               );
                                             },
