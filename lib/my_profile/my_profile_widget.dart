4050b8f11a93400cdb7515c5a812f9cd62ca33b1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-27 21:47:34 +0000

    some changes

--- lib/my_profile/my_profile_widget.dart
+++ lib/my_profile/my_profile_widget.dart
@@ -699,74 +699,79 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
                           ],
                         ),
                       ),
-                      Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          wrapWithModel(
-                            model: _model.sectionHeaderModel2,
-                            updateCallback: () => safeSetState(() {}),
-                            child: SectionHeaderWidget(
-                              title: 'Continue Watching',
-                              type: 'continue watching',
-                              isTrending: false,
-                              isContinueWatching: false,
-                            ),
-                          ),
-                          Container(
-                            height: 180.0,
-                            decoration: BoxDecoration(),
-                            child: Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(
-                                  16.0, 0.0, 16.0, 0.0),
-                              child: StreamBuilder<List<ItemsRecord>>(
-                                stream: queryItemsRecord(
-                                  parent: functions.getContinueWatchingRef(
-                                      FFAppState().activeProfileRef!),
-                                  queryBuilder: (itemsRecord) => itemsRecord
-                                      .orderBy('updated_at', descending: true),
-                                ),
-                                builder: (context, snapshot) {
-                                  // Customize what your widget looks like when it's loading.
-                                  if (!snapshot.hasData) {
-                                    return Center(
-                                      child: SizedBox(
-                                        width: 50.0,
-                                        height: 50.0,
-                                        child: SpinKitPulse(
-                                          color: FlutterFlowTheme.of(context)
-                                              .primary,
-                                          size: 50.0,
-                                        ),
-                                      ),
-                                    );
-                                  }
-                                  List<ItemsRecord> listViewItemsRecordList =
-                                      snapshot.data!;
-
-                                  return ListView.separated(
-                                    padding: EdgeInsets.zero,
-                                    shrinkWrap: true,
-                                    scrollDirection: Axis.horizontal,
-                                    itemCount: listViewItemsRecordList.length,
-                                    separatorBuilder: (_, __) =>
-                                        SizedBox(width: 10.0),
-                                    itemBuilder: (context, listViewIndex) {
-                                      final listViewItemsRecord =
-                                          listViewItemsRecordList[
-                                              listViewIndex];
-                                      return ContinueWatchingCardWidget(
-                                        key: Key(
-                                            'Key4x8_${listViewIndex}_of_${listViewItemsRecordList.length}'),
-                                        continueDoc: listViewItemsRecord,
-                                      );
-                                    },
-                                  );
-                                },
+                      Padding(
+                        padding:
+                            EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 20.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            wrapWithModel(
+                              model: _model.sectionHeaderModel2,
+                              updateCallback: () => safeSetState(() {}),
+                              child: SectionHeaderWidget(
+                                title: 'Continue Watching',
+                                type: 'continue watching',
+                                isTrending: false,
+                                isContinueWatching: false,
                               ),
                             ),
-                          ),
-                        ],
+                            Container(
+                              height: 180.0,
+                              decoration: BoxDecoration(),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    16.0, 0.0, 16.0, 0.0),
+                                child: StreamBuilder<List<ItemsRecord>>(
+                                  stream: queryItemsRecord(
+                                    parent: functions.getContinueWatchingRef(
+                                        FFAppState().activeProfileRef!),
+                                    queryBuilder: (itemsRecord) =>
+                                        itemsRecord.orderBy('updated_at',
+                                            descending: true),
+                                  ),
+                                  builder: (context, snapshot) {
+                                    // Customize what your widget looks like when it's loading.
+                                    if (!snapshot.hasData) {
+                                      return Center(
+                                        child: SizedBox(
+                                          width: 50.0,
+                                          height: 50.0,
+                                          child: SpinKitPulse(
+                                            color: FlutterFlowTheme.of(context)
+                                                .primary,
+                                            size: 50.0,
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                    List<ItemsRecord> listViewItemsRecordList =
+                                        snapshot.data!;
+
+                                    return ListView.separated(
+                                      padding: EdgeInsets.zero,
+                                      shrinkWrap: true,
+                                      scrollDirection: Axis.horizontal,
+                                      itemCount: listViewItemsRecordList.length,
+                                      separatorBuilder: (_, __) =>
+                                          SizedBox(width: 10.0),
+                                      itemBuilder: (context, listViewIndex) {
+                                        final listViewItemsRecord =
+                                            listViewItemsRecordList[
+                                                listViewIndex];
+                                        return ContinueWatchingCardWidget(
+                                          key: Key(
+                                              'Key4x8_${listViewIndex}_of_${listViewItemsRecordList.length}'),
+                                          continueDoc: listViewItemsRecord,
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                       if (_model.isHidden == false)
                         Container(
