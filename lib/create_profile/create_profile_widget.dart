da046ca2b0570eacdd6712267399129ad1d4d284
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-05-11 10:05:06 +0000

    a function added

--- lib/create_profile/create_profile_widget.dart
+++ lib/create_profile/create_profile_widget.dart
@@ -586,8 +586,6 @@ class _CreateProfileWidgetState extends State<CreateProfileWidget> {
                                   (widget.profileDoc != null))
                                 FFButtonWidget(
                                   onPressed: () async {
-                                    await widget.profileDoc!.reference
-                                        .delete();
                                     await actions.deleteContinueWatching(
                                       widget.profileDoc!.reference.id,
                                     );
@@ -595,6 +593,8 @@ class _CreateProfileWidgetState extends State<CreateProfileWidget> {
                                       'my_list',
                                       widget.profileDoc!.reference,
                                     );
+                                    await widget.profileDoc!.reference
+                                        .delete();
 
                                     context.goNamed(
                                         ProfileSelectionWidget.routeName);
